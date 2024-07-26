776cb2abb76fcb83df33146d7007f510cd28e562
阳之幸福 <1689484241@qq.com>
2024-07-26 21:04:53 +0800

    Move App To Src

---
+++ src/pages/index.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const toast = useToast()
+
+
+const hello=()=> {
+  // toast.add({title:"Hello World!"})
+  console.log("Hello ")
+  useToast().add({title: "hello"})
+  console.log("world")
+}
+
+</script>
+
+<template>
+  <UButton label="Show toast" @click="hello" />
+</template>
