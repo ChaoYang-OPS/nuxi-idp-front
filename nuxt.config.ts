dda64a04fb21433b13d6b6ed9e48d6fdcb32661c
Chao Yang <1689484241@qq.com>
2024-07-27 20:33:37 +0800

    Update dependencies and package json

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,5 +3,11 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: "src",
   modules: ['@nuxt/ui'],
+
   // ssr: false
+  ui: {
+    icons: ['solar', 'ri'],
+  },
+
+  compatibilityDate: '2024-07-28'
 })
