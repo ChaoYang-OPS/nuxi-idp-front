d5d02539660c2a5db2fd2494c2330c80bbf69775
Chao Yang <1689484241@qq.com>
2024-08-03 08:17:57 +0800

    Update useToast Testing

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,11 +1,11 @@
 <script setup lang="ts">
-const toast = useToast()
+// const toast = useToast()
 
 
 const hello=()=> {
   // toast.add({title:"Hello World!"})
   console.log("Hello ")
-  useToast().add({title: "hello"})
+  useToast().add({title: "登录成功"})
   console.log("world")
 }
 
@@ -13,4 +13,7 @@ const hello=()=> {
 
 <template>
   <UButton label="Show toast" @click="hello" />
+  <UIcon name="i-heroicons-moon" class="text-4xl"></UIcon>
+  <UIcon name="i-solar-ghost-smile-linear" class="text-4xl"></UIcon>
+  <UIcon name="i-ri-wechat-fill" class="text-4xl"></UIcon>
 </template>
