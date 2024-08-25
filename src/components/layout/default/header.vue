c1f621a8fe93654d5253c77a73b80de1821471db
Chao Yang <1689484241@qq.com>
2024-08-25 16:53:51 +0800

    Code Already Testing Patch

--- src/components/layout/default/header.vue
+++ src/components/layout/default/header.vue
@@ -2,6 +2,31 @@
 
 const colorMode = useColorMode()
 console.log(colorMode)
+const selected = ref(false)
+import { z } from 'zod'
+import type { FormSubmitEvent } from '#ui/types'
+
+const schema = z.object({
+  email: z.string().email('Invalid email'),
+  password: z.string().min(8, 'Must be at least 8 characters')
+})
+
+type Schema = z.output<typeof schema>
+
+const state = reactive({
+  email: undefined,
+  password: undefined
+})
+
+async function onSubmit (event: FormSubmitEvent<Schema>) {
+  // Do something with data
+  console.log(event.data)
+}
+
+const countries = ['United States', 'Canada', 'Mexico']
+
+const country = ref(countries[0])
+
 </script>
 
 <template>
@@ -15,6 +40,72 @@ console.log(colorMode)
       <UButton color="primary" variant="solid" @click="colorMode.preference = 'light'">亮色</UButton>
       <UButton color="primary" variant="solid" @click="colorMode.preference = 'dark'">暗色</UButton>
       <UButton color="primary" variant="solid" @click="colorMode.preference = 'system'">系统</UButton>
+      <UCheckbox v-model="selected" name="notifications" label="Notifications" />
+      <UChip>
+        <UButton icon="i-heroicons-inbox" color="gray" />
+      </UChip>
+
+      <UContainer>
+        <UChip inset>
+          <UAvatar
+              src="https://avatars.githubusercontent.com/u/26598793?v=4"
+              alt="Avatar"
+          />
+        </UChip>
+      </UContainer>
+      <UDivider label="OR" />
+      <UDivider icon="i-simple-icons-github" />
+
+      <UDivider
+          :avatar="{ src: 'https://avatars.githubusercontent.com/u/26598793?v=4' }"
+      />
+      <UDivider
+          label="Nuxt UI"
+          :ui="{ label: 'text-primary-500 dark:text-primary-400' }"
+      />
+      <UForm :schema="schema" :state="state" class="space-y-4" @submit="onSubmit">
+        <UFormGroup label="Email" name="email">
+          <UInput v-model="state.email" />
+        </UFormGroup>
+
+        <UFormGroup label="Password" name="password">
+          <UInput v-model="state.password" type="password" />
+        </UFormGroup>
+
+        <UButton type="submit">
+          Submit
+        </UButton>
+      </UForm>
     </div>
+    <UFormGroup label="Email" required>
+      <UInput placeholder="you@example.com" icon="i-heroicons-envelope" />
+    </UFormGroup>
+    <UFormGroup label="Email" description="We'll only use this for spam.">
+      <UInput placeholder="you@example.com" icon="i-heroicons-envelope" />
+    </UFormGroup>
+    <UFormGroup
+        label="Email"
+        help="We will never share your email with anyone else."
+    >
+      <UInput placeholder="you@example.com" icon="i-heroicons-envelope" />
+    </UFormGroup>
+    <UFormGroup>
+      <template #label>
+        <UAvatar
+            src="https://avatars.githubusercontent.com/u/26598793?v=4"
+            size="3xs"
+        />
+      </template>
+      <template #default>
+        <UInput model-value="ChaoYang-Ops" placeholder="you@example.com" />
+      </template>
+    </UFormGroup>
+    <USelect
+        icon="i-heroicons-magnifying-glass-20-solid"
+        color="white"
+        size="sm"
+        :options="['United States', 'Canada', 'Mexico']"
+        placeholder="Search..."
+    />
   </header>
 </template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -7,6 +7,57 @@ const hello=()=> {
   useToast().add({title: "登录成功"})
 }
 
+const people = [{
+  id: 1,
+  name: 'Lindsay Walton',
+  title: 'Front-end Developer',
+  email: 'lindsay.walton@example.com',
+  role: 'Member'
+}, {
+  id: 2,
+  name: 'Courtney Henry',
+  title: 'Designer',
+  email: 'courtney.henry@example.com',
+  role: 'Admin'
+}, {
+  id: 3,
+  name: 'Tom Cook',
+  title: 'Director of Product',
+  email: 'tom.cook@example.com',
+  role: 'Member'
+}, {
+  id: 4,
+  name: 'Whitney Francis',
+  title: 'Copywriter',
+  email: 'whitney.francis@example.com',
+  role: 'Admin'
+}, {
+  id: 5,
+  name: 'Leonard Krasner',
+  title: 'Senior Designer',
+  email: 'leonard.krasner@example.com',
+  role: 'Owner'
+}, {
+  id: 6,
+  name: 'Floyd Miles',
+  title: 'Principal Designer',
+  email: 'floyd.miles@example.com',
+  role: 'Member'
+}]
+
+
+const links = [{
+  label: 'Home',
+  icon: 'i-heroicons-home',
+  to: '/'
+}, {
+  label: 'Navigation',
+  icon: 'i-heroicons-square-3-stack-3d'
+}, {
+  label: 'Breadcrumb',
+  icon: 'i-heroicons-link'
+}]
+
 </script>
 
 <template>
@@ -14,4 +65,18 @@ const hello=()=> {
   <UIcon name="i-heroicons-moon" class="text-4xl"></UIcon>
   <UIcon name="i-solar-ghost-smile-linear" class="text-4xl"></UIcon>
   <UIcon name="i-ri-wechat-fill" class="text-4xl"></UIcon>
+  <div class="flex items-center space-x-4">
+    <USkeleton class="h-12 w-12" :ui="{ rounded: 'rounded-full' }" />
+    <div class="space-y-2">
+      <USkeleton class="h-4 w-[250px]" />
+      <USkeleton class="h-4 w-[200px]" />
+    </div>
+  </div>
+  <UTable :rows="people" />
+  <UTooltip>
+    <template #text>
+      <span class="italic">Hello World!</span>
+    </template>
+  </UTooltip>
+  <UBreadcrumb :links="links" />
 </template>
